1ed92c832d55a4b508766511391bdac78e83305d
n1032658 <96579206+n1032658@users.noreply.github.com>
2022-12-29 16:30:43 +0000

    Update kubesec-scan.sh

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
-scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score)
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
 
-echo $scan_score
+echo "Scan Score : $scan_score"
 
-if [[ "${scan_score}" -ge 4 ]]; then
-  echo "Score is $scan_score"
-  echo "Kubesec Scan $scan_message"
-else
-  echo "Score is $scan_score"
-  echo "Kubernetes template scanning failed because score is less than or equal to $scan_score"
-  exit 1;
-fi
+	if [[ "${scan_score}" -ge 5 ]]; then
+	    echo "Score is $scan_score"
+	    echo "Kubesec Scan $scan_message"
+	else
+	    echo "Score is $scan_score, which is less than or equal to 5."
+	    echo "Scanning Kubernetes Resource has Failed"
+	    exit 1;
+	fi;
