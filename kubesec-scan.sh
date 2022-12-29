3e3769885cfe16caad09bfa300c1d44279512df3
n1032658 <96579206+n1032658@users.noreply.github.com>
2022-12-29 16:36:47 +0000

    Update kubesec-scan.sh

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -6,11 +6,11 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https:/
 
 echo "Scan Score : $scan_score"
 
-	if [[ "${scan_score}" -ge 5 ]]; then
-	    echo "Score is $scan_score"
-	    echo "Kubesec Scan $scan_message"
-	else
-	    echo "Score is $scan_score, which is less than or equal to 5."
-	    echo "Scanning Kubernetes Resource has Failed"
-	    exit 1;
-	fi;
+if [[ "${scan_score}" -ge 5 ]]; then
+    echo "Score is $scan_score"
+    echo "Kubesec Scan $scan_message"
+else
+    echo "Score is $scan_score, which is less than or equal to 5."
+    echo "Scanning Kubernetes Resource has Failed"
+    exit 1;
+fi;
